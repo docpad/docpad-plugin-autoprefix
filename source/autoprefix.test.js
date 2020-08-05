b764b14b12fcdc582325fc3c1f7b827a62ee3125
Benjamin Lupton <b@lupton.cc>
2020-08-05 12:47:50 +1000

    Delete autoprefix.test.js
    no need for this anymore

--- source/autoprefix.test.js
+++
@@ -1,7 +0,0 @@
-// Test our plugin using DocPad's Testers
-'use strict'
-
-require('docpad').require('testers').test({
-	pluginPath: require('path').join(__dirname, '..'),
-	testerClass: 'RendererTester'
-})
