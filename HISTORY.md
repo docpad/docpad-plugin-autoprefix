90dab4c8e5d46bc273dd3ca85479a3a79147dc75
Benjamin Lupton <b@lupton.cc>
2020-08-05 13:14:23 +1000

    v2.3.0 - boundation, changelog, version bump

--- HISTORY.md
+++ HISTORY.md
@@ -1,13 +1,21 @@
 # History
 
+## v2.3.0 2020 August 5
+
+-   Updated dependencies, [base files](https://github.com/bevry/base), and [editions](https://editions.bevry.me) using [boundation](https://github.com/bevry/boundation)
+-   Minimum required node version changed from `node: >=0.12` to `node: >=10` to keep up with mandatory ecosystem changes
+
 ## v2.2.0 2018 February 7
-- Updated base files
+
+-   Updated base files
 
 ## v2.1.0 2017 April 16
-- Updated base files
-- Updated dependencies
-- Converted from CoffeeScript to ESNext
-- Bumped minimum node version from 0.4 to 0.12 to comply with node ecosystem
+
+-   Updated base files
+-   Updated dependencies
+-   Converted from CoffeeScript to ESNext
+-   Bumped minimum node version from 0.4 to 0.12 to comply with node ecosystem
 
 ## v2.0.0 2014 January 25
-- Initial working version
+
+-   Initial working version
--- source/index.js
+++ source/index.js
@@ -2,47 +2,52 @@
 
 // Import
 const autoprefixer = require('autoprefixer')
-const postcss      = require('postcss')
+const postcss = require('postcss')
 
 // Export Plugin
 module.exports = function (BasePlugin) {
 	// Define Plugin
 	return class AutoprefixPlugin extends BasePlugin {
 		// Plugin name
-		get name () {
+		get name() {
 			return 'autoprefix'
 		}
 
 		// Render
-		autoprefix (docpad, content, next) {
-			postcss([autoprefixer]).process(content).then(function (result) {
-				result.warnings().forEach(function (warn) {
-					docpad.log('warn', warn.toString())
+		autoprefix(docpad, content, next) {
+			postcss([autoprefixer])
+				.process(content)
+				.then(function (result) {
+					result.warnings().forEach(function (warn) {
+						docpad.log('warn', warn.toString())
+					})
+					next(null, result.css)
 				})
-				next(null, result.css)
-			}).catch(next)
+				.catch(next)
 		}
 
 		// Render .autoprefix extension
-		render (opts, next) {
+		render(opts, next) {
 			// Prepare
 			const { inExtension, outExtension } = opts
 
 			// Check extensions
-			if (inExtension === 'autoprefix' && ['css', null].indexOf(outExtension) !== -1) {
+			if (
+				inExtension === 'autoprefix' &&
+				['css', null].indexOf(outExtension) !== -1
+			) {
 				this.autoprefix(this.docpad, opts.content, function (err, result) {
 					if (err) return next(err)
 					opts.content = result
 					next()
 				})
-			}
-			else {
+			} else {
 				next()
 			}
 		}
 
 		// Render `autoprefix: true` header
-		renderDocument (opts, next) {
+		renderDocument(opts, next) {
 			// Prepare
 			const { file } = opts
 
@@ -53,8 +58,7 @@ module.exports = function (BasePlugin) {
 					opts.content = result
 					next()
 				})
-			}
-			else {
+			} else {
 				next()
 			}
 		}
